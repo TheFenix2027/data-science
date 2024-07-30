42c3ce110719c6d73e1efae67566ce02af9eb385
TheFenix2027 <95001663+TheFenix2027@users.noreply.github.com>
2024-07-29 20:48:33 -0400

    Data-science
    Este trabajo es sobre Data Science usando Python.

---
+++ Data Types.py
@@ -0,0 +1,7 @@
+nombreEmpleado = "David"
+edad = 25
+peso = 135.6
+print(type(nombreEmpleado))
+print(type(edad))
+print(type(peso))
+print(type(peso))
---
+++ Operadores.py
@@ -0,0 +1,30 @@
+#Operadores de asignación
+a=5
+b=2
+a+=b
+a-=b
+a*=b
+a/=b
+
+#Operadores de comparasión
+x=5
+y=7
+print(x < y)
+print(x <= y)
+print(x > y)
+print(x >= y)
+print(x == y)
+
+#Operadores lógicos
+print(x < y and x > y)
+print(x < y or x > y)
+print(not(x < y or x > y))
+
+#Mmbership operator
+c = [1,2,3,4,5]
+print(2 in c)
+print(2 not in c)
+
+#Operadores de identidad
+print(a is b)
+print(a is not b)
